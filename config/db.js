0358c5fbab58575e0c48b3abe769c2d18dfb3779
Rajneesh Kumar <rajneeshkumar.mern@gmail.com>
2026-04-22 01:26:44 +0530

    Fixed POST route removed auth

--- config/db.js
+++ config/db.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI exists:", !!process.env.MONGO_URI)
+
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -9,6 +11,7 @@ const connectDB = async () => {
 
     console.log(`MongoDB connected: ${conn.connection.host}`)
   } catch (error) {
+    console.error("FULL ERROR:", error)
     console.error(`Error connecting to MongoDB: ${error.message}`)
     process.exit(1)
   }
