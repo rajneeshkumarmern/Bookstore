9e83c9061f533bca89c613ba45b2e0812c61a328
Rajneesh Kumar <rajneeshkumar.mern@gmail.com>
2026-04-22 09:19:24 +0530

    serve images static

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.use('/images', express.static('public/images'));
 
 // Routes
 const bookRoutes = require("./routes/bookRoutes");
